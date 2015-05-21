d61f659ff301c9794f351e3ee4e27a2bbe6b9e2c
Mike Funk <mike@mikefunk.com>
2015-05-21 10:37:36 -0700

    added gulp watch command although you can do the same with webpack -w

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,9 +2,19 @@ var gulp = require('gulp');
 var webpack = require('webpack');
 var webpackOptions = require('./webpack.config');
 
+var paths = {
+	js: [
+		'app.js'
+	]
+};
+
 gulp.task('webpack', function (callback) {
 	webpack(webpackOptions, function (error, stats) {
 		// if error...
 		callback();
 	});
+});
+
+gulp.task('watch', function (callback) {
+	gulp.watch(paths.js, ['webpack']);
 });
